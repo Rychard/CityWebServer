667f84ce3a36371c2d7a4e8590127321c0d9c790
Joshua Shearer <joshua.a.shearer@gmail.com>
2015-03-24 18:40:12 -0400

    Remove unused namespace.

--- CityWebServer/Helpers/ApacheMimeTypes.cs
+++ CityWebServer/Helpers/ApacheMimeTypes.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 // Source: https://raw.githubusercontent.com/cymen/ApacheMimeTypesToDotNet/master/ApacheMimeTypes.cs
 namespace ApacheMimeTypes
