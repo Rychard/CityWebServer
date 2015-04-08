87f660627f7f7cdc6f96e52b94aa781742021329
nezroy <nezroy@gmail.com>
2015-04-08 14:43:16 -0600

    update sample extension to plugin loader arch

--- SampleWebServerExtension/SampleRequestHandler.cs
+++ SampleWebServerExtension/SampleRequestHandler.cs
@@ -9,7 +9,7 @@ namespace SampleWebServerExtension
     public class SampleRequestHandler : RequestHandlerBase
     {
         public SampleRequestHandler(IWebServer server)
-            : base(server, new Guid("1a255904-bf72-406e-b5e2-c5a43fdd9bba"), "Sample", "Rychard", 100, "/Sample")
+            : base(server, "/")
         {
         }
 
--- SampleWebServerExtension/UserModInfo.cs
+++ SampleWebServerExtension/UserModInfo.cs
@@ -1,18 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
 using ICities;
+using CityWebServer.Extensibility;
 
 namespace SampleWebServerExtension
 {
-    public class UserModInfo : IUserMod
+    public class UserModInfo : IUserMod, ICityWebPlugin
     {
         public String Name
         {
             get { return "Sample Web Server Extension"; }
         }
 
+        public String PluginName
+        {
+            get { return "Sample Extension"; }
+        }
+
+        public String PluginAuthor
+        {
+            get { return "Rychard"; }
+        }
+
+        public String PluginID
+        {
+            get { return "sample"; }
+        }
+
         public String Description
         {
             get { return "Adds a sample page to the integrated web server.  Doesn't do anything without it!"; }
+        }
+
+        public List<IRequestHandler> GetHandlers(IWebServer server)
+        {
+            List<IRequestHandler> h = new List<IRequestHandler>();
+            h.Add(new SampleRequestHandler(server));
+            return h;
         }
     }
 }
