dedf771b5f27d73ce9ebe9bbb8c46bc051090dd7
Alexis Caldwell <46656008+acaldwell1997@users.noreply.github.com>
2022-11-09 00:54:31 -0600

    Update script.js

--- CityWebServer/wwwroot/script.js
+++ CityWebServer/wwwroot/script.js
@@ -128,3 +128,114 @@ function updateChart(vm, chart)
     }
     chart.redraw();
 }
+
+function filterTable() {
+  // Declare variables
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("searchInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("mainDataTable");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+function sortTable(n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("mainDataTable");
+  switching = true;
+  //Set the sorting direction to ascending:
+  dir = "asc"; 
+  /*Make a loop that will continue until
+  no switching has been done:*/
+  while (switching) {
+    //start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    /*Loop through all table rows (except the
+    first, which contains table headers):*/
+    for (i = 1; i < (rows.length - 1); i++) {
+      //start by saying there should be no switching:
+      shouldSwitch = false;
+      /*Get the two elements you want to compare,
+      one from current row and one from the next:*/
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+      /*check if the two rows should switch place,
+      based on the direction, asc or desc:*/
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          //if so, mark as a switch and break the loop:
+          shouldSwitch= true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          //if so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      /*If a switch has been marked, make the switch
+      and mark that a switch has been done:*/
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      //Each time a switch is done, increase this count by 1:
+      switchcount ++;      
+    } else {
+      /*If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again.*/
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}
+
+    var endpoint = "./CityInfo";
+    var viewModel;
+    var chart;
+    var chart2;
+    var initialized = false;
+    var lastDate;
+    let history = [];
+    
+    
+    // Update the data once every second.
+    window.setInterval(function ()
+    {
+        $.getJSON(endpoint, function(data) {
+            if (initialized) {
+                ko.mapping.fromJS(data, viewModel);
+                if (lastDate !== viewModel.Time()) {
+                    updateChart(viewModel, chart);
+                    lastDate = viewModel.Time();
+                } else {
+                    console.log("Same Date: " + lastDate);
+                }
+            } else {
+                initialized = true;
+                viewModel = ko.mapping.fromJS(data);
+                ko.applyBindings(viewModel);
+                chart = initializeSplineChart(viewModel);
+            }
+        });
+    }, 2000); // Every two seconds.
+
+	function appendHistoryTable(){
+		console.log(history)
+	}
